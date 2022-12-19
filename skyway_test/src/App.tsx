e04b1c5103bad3f8273c45b9161f0a073ec997fb
shuto-416 <hsjudo416@gmail.com>
2022-12-19 17:00:56 +0900

    ファイル送信機能追加

--- skyway_test/src/App.tsx
+++ skyway_test/src/App.tsx
@@ -8,7 +8,8 @@ function App() {
   const [myId, setMyId] = useState<string>('')
   const [chatId, setChatId] = useState<string>('')
   const [myChat, setMyChat] = useState<string>('')
-  const [history, setHistory] = useState<Array<string>>([])
+  const [history, setHistory] = useState<Array<any>>([])
+  const [file, setFile] = useState<File | null>(null)
   const [dataConnection, setDataConnection] = useState<DataConnection>()
 
   useEffect(() => {
@@ -16,7 +17,21 @@ function App() {
       console.log('Peer open', peer.id)
       setMyId(peer.id)
     })
-  }, [peer])
+
+    peer.on("connection", (data) => {
+      data.on('open', () => {
+        console.log('data connection open')
+        setDataConnection(data)
+      })
+
+      data.on('data', (chat: any) => {
+        console.log('data', chat)
+        history.push(chat)
+        setHistory(history)
+      })
+    })
+
+  }, [])
 
   const connectChat = () => {
     const dataConn = peer.connect(chatId)
@@ -32,21 +47,6 @@ function App() {
     })
   }
 
-  useEffect(() => {
-    peer.on("connection", (data) => {
-      data.on('open', () => {
-        console.log('data connection open')
-        setDataConnection(data)
-      })
-
-      data.on('data', (chat: any) => {
-        console.log('data', chat)
-        history.push(chat)
-        setHistory(history)
-      })
-    })
-  }, [peer])
-
   const sendChat = () => {
     if (dataConnection) {
       console.log('send chat', myChat)
@@ -54,7 +54,31 @@ function App() {
     }
   }
 
+  const sendFile = async () => {
+    if (dataConnection && file) {
+      console.log('send chat', file)
+      const blob = new Blob([file])
+      const link = URL.createObjectURL(blob)
+      dataConnection.send({ link, name: file.name, })
+    }
+  }
 
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files && files[0]) {
+      setFile(files[0])
+    }
+  }
+
+  const downLoadFile = (sunFile: File) => {
+    console.log('sended', sunFile)
+    const blob = new Blob([sunFile]) // Blob オブジェクトの作成
+    const link = document.createElement('a')
+    link.download = sunFile.name; // ダウンロードファイル名称
+    link.href = URL.createObjectURL(blob); // オブジェクト URL を生成
+    link.click(); // クリックイベントを発生させる
+    URL.revokeObjectURL(link.href)
+  }
 
   return (
     <div className="App">
@@ -67,14 +91,20 @@ function App() {
         <button onClick={(e) => sendChat()}>send</button>
       </div>
       <div>
+        <input name="file" type="file" onChange={onChangeFile} />
+        <input type="button" disabled={!file} value="送信" onClick={sendFile} />
+      </div>
+      <div>
         {history.length !==0 && history.map((his, i) => (
-          <p key={i}>
-            {his}<br />
-          </p>
+          // <p key={i}>
+          //   {his}<br />
+          //   <button key={i} onClick={() => downLoadFile(his)}>ダウンロード</button>
+            <a key={i} download={his.name} href={his.link}>ダウンロード</a>
+          // </p>
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
